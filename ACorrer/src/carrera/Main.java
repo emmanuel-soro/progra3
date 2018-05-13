f697a865173d9235324a852223528dce7f363b59
Emmanuel <Emmanuel@Thermaltake>
2018-05-13 15:13:46 -0300

    Agregado caso de stress

--- ACorrer/src/carrera/Main.java
+++ ACorrer/src/carrera/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
-
 		Carrera carrera = new Carrera("01.in", "01.out");
 
 		long startTime = System.nanoTime();
@@ -14,13 +12,86 @@ public class Main {
 
 		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
 
-		Carrera carrera2 = new Carrera("01.in", "01-mejorado.out");
+		carrera = new Carrera("02.in", "02.out");
 
 		startTime = System.nanoTime();
-		carrera2.resolverMejorado();
+		carrera.resolver();
 		estimatedTime = System.nanoTime() - startTime;
 
-		System.out.println("Metodo resolver mejorado demoró:" + estimatedTime + " nanosegundos.");
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("03.in", "03.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("04.in", "04.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("05.in", "05.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("06.in", "06.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("07.in", "07.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("08.in", "08.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("09.in", "09.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("10.in", "10.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
+		carrera = new Carrera("11.in", "11.out");
+
+		startTime = System.nanoTime();
+		carrera.resolver();
+		estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Metodo resolver demoró:" + estimatedTime + " nanosegundos.");
+
 	}
 
 }
